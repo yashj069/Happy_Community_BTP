65a77164be78bfe85d452026cb078351a82b50c3
Yash Jindal <yashjindal069@gmail.com>
2023-01-05 10:36:55 +0530

    firebase data changes

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,33 +1,56 @@
-// @flow 
-import * as React from 'react';
+// @flow
+import * as React from "react";
 import "../card/Card.css";
-import { toast } from 'react-toastify';
-import { useAuth } from '../../hooks/useAuth';
-import { addToMyCommunity,deleteMyCommunity } from '../../services/firebase';
-
+import { toast } from "react-toastify";
+import { useAuth } from "../../hooks/useAuth";
+import { addToMyCommunity, deleteMyCommunity } from "../../services/firebase";
 
 const Card = (props) => {
-    const {user}= useAuth();
-    return (
-        <div>
-            <div class="card border" style={{padding:"10px", width : "50vh",minHeight:"250px"}}>
-                <div class="card-body d-flex flex-column" style={{marginTop : "30px"}}>
-                    <span className='cardInnerBox'>
-                        <h5 class="card-title" className='cardInnerBoxHeading1'>{props.room.title}</h5>
-                        <p class="card-text" className='cardInnerBoxText1'>{props.room.desc}</p>
-                        
-                        {
-                            props.isJoin && <button type="button" onClick={()=>{deleteMyCommunity(props.docid,user);props.callFunc();}} className="cardJoinButton2">LEAVE</button>
-                        }
-                        {
-                            (!props.isJoin) && <button type="button" onClick={()=>{addToMyCommunity(props.room,user);props.callFunc();}} className="cardJoinButton1">JOIN</button>
-                        }
-                    </span>
-                </div>
-            </div>
- 
+  const { user } = useAuth();
+  return (
+    <div>
+      <div
+        class="card border"
+        style={{ padding: "10px", width: "50vh", minHeight: "250px" }}
+      >
+        <div class="card-body d-flex flex-column" style={{ marginTop: "30px" }}>
+          <span className="cardInnerBox">
+            <h5 class="card-title" className="cardInnerBoxHeading1">
+              {props.room.title}
+            </h5>
+            <p class="card-text" className="cardInnerBoxText1">
+              {props.room.desc}
+            </p>
+
+            {props.isJoin && (
+              <button
+                type="button"
+                onClick={() => {
+                  deleteMyCommunity(props.docid, user);
+                  props.callFunc();
+                }}
+                className="cardJoinButton2"
+              >
+                LEAVE
+              </button>
+            )}
+            {!props.isJoin && (
+              <button
+                type="button"
+                onClick={() => {
+                  addToMyCommunity(props.room, user);
+                  props.callFunc();
+                }}
+                className="cardJoinButton1"
+              >
+                JOIN
+              </button>
+            )}
+          </span>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Card;
